9a2f8cf9424c47d940632e97ab23097966adf86d
Jenn Basalone <jbasalone@gmail.com>
2024-12-08 20:24:05 -0800

    error handling around quarantine-logs

--- src/commands/chat/staff_channel_assign-prefix.ts
+++ src/commands/chat/staff_channel_assign-prefix.ts
@@ -22,27 +22,27 @@ export = {
     async execute(message: Message): Promise<void> {
 	 try{
 		 if(message.channel.type !== ChannelType.GuildText) return;
-	    let stringContent = message.content.toString()
-		if(!stringContent.includes("#")) {
+	    	let stringContent = message.content.toString()
+		 if(!stringContent.includes("#")) {
                     await message.reply('please specify a channel name ex. # channelname')
                	     return;
-		}
-	    let newName = stringContent.split('#')
-	    let owner = message.mentions.users.first()
-	    if(!owner){
+		 }
+		 let newName = stringContent.split('#')
+		 let owner = message.mentions.users.first()
+		 if(!owner){
                    await message.reply('please specify a valid @user')
        		           return;
-	    }
-	    let ownerid = `${owner.id}`
-	    let channelCheck = checkisland(owner, message.guild.id)
-	    let emojiName;
-	    let channelName;
-	    let channelWord;
-		let ownerRole = message.guild.roles.cache.get('1147864509344661644')
+		 }
+		 let ownerid = `${owner.id}`
+		 let channelCheck = checkisland(owner, message.guild.id)
+		 let emojiName;
+		 let channelName;
+		 let channelWord;
+		 let ownerRole = message.guild.roles.cache.get('1147864509344661644')
 		 let modRole = '1148992217202040942'
 		 let verifiedRole = '1143236724718317673'
-	    let memberTarget = message.guild.members.cache.get(owner.id)
-	    let boosterRole = "1142141020218347601"
+		 let memberTarget = message.guild.members.cache.get(owner.id)
+		 let boosterRole = "1142141020218347601"
 		 let staffRole = "1148992217202040942"
 		 let staffParent = "1140190313915371530"
 		 let boosterParent = "1147909067172483162"
@@ -51,21 +51,20 @@ export = {
 		 let adventureTrails = "1147909373180530708"
 		 let tropicalLounge = "1147909539413368883"
 		 let parkPeaks = "1147909156196593787"
-	    let myamari =  await amariclient.getUserLevel(message.guild.id, ownerid)
-	    let level = parseInt(`${myamari.level}`)
-	    const regex = emojiRegex();
-	    let emojiCount = 0
-	    let serverId = message.guild.id
+		 let myamari;
+		 let level;
+	     const regex = emojiRegex();
+	     let emojiCount = 0
+	     let serverId = message.guild.id
 	     
-	    if(newName[1].includes("<")){
+		 if(newName[1].includes("<")){
 		    await message.reply('You can only use stanard emojis in channel names')
                     return;
             }
             
             const serverCheck = Array.from(checkisland(owner, serverId));
-		 console.log(channelCheck)
 
-	    if(serverCheck) {
+		 if(serverCheck) {
 		    //@ts-ignore
 		    const isAdded = serverCheck && serverCheck.some((user) => user.server  === serverId)
 
@@ -76,7 +75,7 @@ export = {
                         	await message.reply({embeds: [embed]})
                         	return;
 		    }
-            }
+		 }
 	    let progress_bar = await message.channel.send('=>..')
 
 	    for (const match of String(newName).matchAll(regex)) {
@@ -84,20 +83,20 @@ export = {
 	     	    	  ++emojiCount 
             	}
 
-            if(emojiCount >= 2) {
+		if(emojiCount >= 2) {
 		    message.reply("please only use 1 emoji, further name changes can be made with `ep name` \n*required format*: `# <emoji> channel name` or `# channelname`")
 		    return;
 	    }
 
-            if(emojiName) {
+		if(emojiName) {
                  channelWord = String(newName).split(`${emojiName}`)[1].trimStart();
                  channelName = channelWord
-            }else{
-		 channelName = String(newName).split(`,`)[1].trimStart();
-		 channelName = channelName.replace(/>/g, '')
+		}else{
+			channelName = String(newName).split(`,`)[1].trimStart()
+			channelName = channelName.replace(/>/g, '')
 	    }
 
-	   let channelExists =  await message.guild.channels.cache.find(channel => channel.id === `${channelName}`)
+		let channelExists =  await message.guild.channels.cache.find(channel => channel.id === `${channelName}`)
 		 let channelIHAZ = 0
 
 	   if(channelExists){
@@ -105,33 +104,35 @@ export = {
 	   }
 	       
 
-	    await progress_bar.edit('====>...')
+	   await progress_bar.edit('====>...')
 	    
 	   
 	    let channel;
 	    if((channelIHAZ === 1) && (message.guild.id ==="1135995107842195550") ) {
 		    channel = await message.guild.channels.cache.find(channel => channel.id === `${channelName}`) as TextChannel;
-		    if(memberTarget.roles.cache.has(staffRole)){
-                        await channel.setParent(staffParent);
-                    }else if(memberTarget.roles.cache.has(boosterRole)){
-                        await channel.setParent(boosterParent);
-                    }else if((level >= 40) && (level <= 59)){
-                        await channel.setParent(parkPavilion);
-                    }else if((level >= 60) && (level <= 79)){
-                        await channel.setParent(adventureTrails);
-                    }else if((level >= 80) && (level <= 119)){
-                        await channel.setParent(tropicalLounge);
-                    }else if(level >= 120) {
-                        await channel.setParent(parkPeaks);
-                   }
+			myamari =  await amariclient.getUserLevel(message.guild.id, ownerid)
+			level = parseInt(`${myamari.level}`)
+			if(memberTarget.roles.cache.has(staffRole)){
+				await channel.setParent(staffParent);
+			}else if(memberTarget.roles.cache.has(boosterRole)){
+				await channel.setParent(boosterParent);
+			}else if((level >= 40) && (level <= 59)){
+				await channel.setParent(parkPavilion);
+			}else if((level >= 60) && (level <= 79)){
+				await channel.setParent(adventureTrails);
+			}else if((level >= 80) && (level <= 119)){
+				await channel.setParent(tropicalLounge);
+			}else if(level >= 120) {
+				await channel.setParent(parkPeaks);
+			}
 		    await progress_bar.edit('========>...')
-               	    await channel.lockPermissions()
-               	    await channel.permissionOverwrites.edit(
-                                                `${owner.id}`,
-                                                {SendMessages:true,
-                                                ViewChannel:true}
+			await channel.lockPermissions()
+			await channel.permissionOverwrites.edit(
+				`${owner.id}`,
+				{SendMessages:true, ViewChannel:true}
                                                 );
-               	    await progress_bar.edit('==========>.')
+			await progress_bar.edit('==========>.')
+
 		    if(await createisland(`${owner.id}`, `${channel.id}`, `${serverId}`) === 'Created!') {
                         let channelOwner = message.guild.members.cache.get(`${owner.id}`)
                         await enableevents(`${channel.id}`)
@@ -140,112 +141,58 @@ export = {
                         await message.reply("Something happened adding user to the database, contact a dev")
                         return;
                      }
+
             }else if((channelIHAZ === 0) && (message.guild.id ==="1135995107842195550")){
-		    channel = await message.guild.channels.create({
+				myamari =  await amariclient.getUserLevel(message.guild.id, ownerid)
+				level = parseInt(`${myamari.level}`)
+		    	channel = await message.guild.channels.create({
                          	name: `${channelName}`,
                         	type: ChannelType.GuildText,
                         	parent: "1147909200924643349",
                         })
 
-		    if(emojiName) {
+		    	if(emojiName) {
                  	channelWord = String(newName).split(`${emojiName}`)[1].trimStart();
                  	channelName = " "
                  	channelName = String(channelName).concat(String(emojiName) + '・' + String(channelWord));
                  	await channel.edit({name: channelName})
-            	    } else {
+				} else {
                     	channelWord = String(newName).split(',')[1].trimStart();
                     	channelName = " "
                     	channelName = String(channelName).concat('・' + String(channelWord))
                     	await channel.edit({name: channelName})
             	    }
-		    if(memberTarget.roles.cache.has(staffRole)){
-                    	await channel.setParent(staffParent);
-            	    }else if(memberTarget.roles.cache.has(boosterRole)){
-                    	await channel.setParent(boosterParent);
-            	    }else if((level >= 40) && (level <= 59)){
-                     	await channel.setParent(parkPavilion);
-            	    }else if((level >= 60) && (level <= 79)){
-                    	await channel.setParent(adventureTrails);
-            	    }else if((level >= 80) && (level <= 119)){
-                    	await channel.setParent(tropicalLounge);
-            	    }else if(level >= 120) {
-                    	await channel.setParent(parkPeaks);
-                   }
-               	   await progress_bar.edit('========>...')
-	           await channel.lockPermissions()
-                   await channel.permissionOverwrites.edit(
-                                                `${owner.id}`,
-                                                {SendMessages:true,
-                                                ViewChannel:true}
+		    	if(memberTarget.roles.cache.has(staffRole)){
+					await channel.setParent(staffParent);
+				}else if(memberTarget.roles.cache.has(boosterRole)){
+					await channel.setParent(boosterParent);
+				}else if((level >= 40) && (level <= 59)){
+					await channel.setParent(parkPavilion);
+				}else if((level >= 60) && (level <= 79)){
+					await channel.setParent(adventureTrails);
+				}else if((level >= 80) && (level <= 119)){
+					await channel.setParent(tropicalLounge);
+				}else if(level >= 120) {
+					await channel.setParent(parkPeaks);
+				}
+				await progress_bar.edit('========>...')
+				await channel.lockPermissions()
+				await channel.permissionOverwrites.edit(
+					`${owner.id}`,
+					{SendMessages:true, ViewChannel:true}
                                                 );
-	           await progress_bar.edit('==========>.')
+				await progress_bar.edit('==========>.')
 
-                   if(await createisland(`${owner.id}`, `${channel.id}`, `${serverId}`) === 'Created!') {
+				if(await createisland(`${owner.id}`, `${channel.id}`, `${serverId}`) === 'Created!') {
                 	let channelOwner = message.guild.members.cache.get(`${owner.id}`)
                 	await enableevents(`${channel.id}`)
                 	await channelOwner.roles.add(ownerRole)
 		  
-               	   }else{
+				}else{
                         await message.reply("Something happened adding user to the database, contact a dev")
                         return;
             	  }	
 
-	    }else if((channelIHAZ === 1) && (message.guild.id ==="801822272113082389") ) {
-                    channel = await message.guild.channels.cache.find(channel => channel.id === `${channelName}`) as TextChannel;
-		    ownerRole = message.guild.roles.cache.get('1262566008405622879')
-                    await progress_bar.edit('========>...')
-                    await channel.lockPermissions()
-                    await channel.permissionOverwrites.edit(
-                                                `${owner.id}`,
-                                                {SendMessages:true,
-                                                ViewChannel:true}
-                                                );
-                    await progress_bar.edit('==========>.')
-                    if(await createisland(`${owner.id}`, `${channel.id}`, `${serverId}`) === 'Created!') {
-                        let channelOwner = message.guild.members.cache.get(`${owner.id}`)
-                        await enableevents(`${channel.id}`)
-                        await channelOwner.roles.add(ownerRole)
-                     }else{
-                        await message.reply("Something happened adding user to the database, contact a dev")
-                        return;
-                     }
-	    }else if((channelIHAZ === 0) && (message.guild.id === "801822272113082389")){
-                    channel = await message.guild.channels.create({
-                                name: `${channelName}`,
-                                type: ChannelType.GuildText,
-                                parent: "1192108950049529906",
-					})
-			ownerRole = message.guild.roles.cache.get('1262566008405622879')
-
-                    if(emojiName) {
-                        channelWord = String(newName).split(`${emojiName}`)[1].trimStart();
-                        channelName = " "
-                        channelName = String(channelName).concat('『'+String(emojiName)+'』'+String(channelWord));
-                        await channel.edit({name: channelName})
-                    } else {
-                        channelWord = String(newName).split(',')[1].trimStart();
-                        channelName = " "
-                        channelName = String(channelName).concat('『』'+String(channelWord))
-                        await channel.edit({name: channelName})
-                    }
-                   await progress_bar.edit('========>...')
-                   await channel.lockPermissions()
-                   await channel.permissionOverwrites.edit(
-                                                `${owner.id}`,
-                                                {SendMessages:true,
-                                                ViewChannel:true}
-                                                );
-                   await progress_bar.edit('==========>.')
-
-                   if(await createisland(`${owner.id}`, `${channel.id}`, `${serverId}`) === 'Created!') {
-                        let channelOwner = message.guild.members.cache.get(`${owner.id}`)
-                        await enableevents(`${channel.id}`)
-                        await channelOwner.roles.add(ownerRole)
-
-                   }else{
-                        await message.reply("Something happened adding user to the database, contact a dev")
-                        return;
-		   }
 		}else if((channelIHAZ === 0) && (message.guild.id === "1113339391419625572")) {
 			channel = await message.guild.channels.create({
 				name: `${channelName}`,
@@ -309,7 +256,7 @@ export = {
 		}
 
 	    
-           let embed1 = new EmbedBuilder()
+		let embed1 = new EmbedBuilder()
 		.setTitle("Channel Manager: Channel Creation")
 		.setDescription(`${owner} is assigned channel: <#${channel.id}>
 
@@ -321,9 +268,8 @@ export = {
 			    {name:"**--Channel Created By--**", value: `<@${message.author.id}>`, inline: true},
 		 )
 
-	    
-           await message.reply({embeds: [embed1]}); 
-           await channel.send({embeds: [embed2]});
+		 await message.reply({embeds: [embed1]});
+		await channel.send({embeds: [embed2]});
 
            
 	         
