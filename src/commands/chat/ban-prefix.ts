a531a3577fc22564cf8ed550ec76131acf2620d5
Jenn Basalone <jbasalone@gmail.com>
2025-05-23 23:03:08 -0700

    bug fixes for ban-prefix just formatting staff_user_kick.ts takes numerical ids

--- src/commands/chat/ban-prefix.ts
+++ src/commands/chat/ban-prefix.ts
@@ -28,7 +28,7 @@ export default new PrefixCommand({
                         '1147909539413368883',
                         '1147909373180530708',
                         '1147909282201870406',
-                        '1147909200924643349',
+                        '1147909200924643349', // skater park
                         '1140190313915371530',
                         '1203928376205905960',
                         '1232728117936914432',
@@ -36,7 +36,7 @@ export default new PrefixCommand({
                         '1192108950049529906',
                         '1225165761920630845',
                         '966458661469839440',
-    		                 '808109909266006087',
+                        '808109909266006087',
                         '825060923768569907',
                         '1113414355669753907',// epic wonderland play land staff
                         '1115772256052846632', /// epic wonderland staff
@@ -61,118 +61,117 @@ export default new PrefixCommand({
 
     async execute(message: Message): Promise<void> {
 	try{
-		if(message.channel.type !== ChannelType.GuildText) return;
-                // This whole Block checks for the channel owner and if not channel owner
-                 // if its not the channel owner, checks for the staff role
-                 // if user is a staff member, they can run the command
-                 // if user is a channel owner or a cowner on the channel / mentioned channel,
-                 // then they are authorized.
+		  if(message.channel.type !== ChannelType.GuildText) return;
+        // This whole Block checks for the channel owner and if not channel owner
+         // if its not the channel owner, checks for the staff role
+         // if user is a staff member, they can run the command
+         // if user is a channel owner or a cowner on the channel / mentioned channel,
+         // then they are authorized.
 
-                let getOwner = await isOwner(message.author.id)
-                let checkStaff = await  message.guild.members.cache.get(message.author.id)
-                let channel = message.channel.id
-		        let serverId = message.guild.id
+      let getOwner = await isOwner(message.author.id)
+      let checkStaff = await  message.guild.members.cache.get(message.author.id)
+      let channel = message.channel.id
+      let serverId = message.guild.id
 
-                //handles null values
-                let checkOwner = getOwner && getOwner.some((authorized) => authorized.channel === channel)
+        //handles null values
+      let checkOwner = getOwner && getOwner.some((authorized) => authorized.channel === channel)
 
-		const modRoleList: { [key: string]: string } = {
-                "1135995107842195550": "1148992217202040942", // epic park staff
-                '1113339391419625572':'1113407924409221120', // epic wonderland staff
-                "839731097473908767": "845499229429956628", // blackstone staff royal guards
-                "871269916085452870": "871393325389844521", // Luminescent Staff
+      const modRoleList: { [key: string]: string } = {
+        "1135995107842195550": "1148992217202040942", // epic park staff
+        '1113339391419625572':'1113407924409221120', // epic wonderland staff
+        "839731097473908767": "845499229429956628", // blackstone staff royal guards
+        "871269916085452870": "871393325389844521", // Luminescent Staff
 
-    };
+        };
 
-                const roleId = Object.entries(modRoleList).find(([key, val]) => key === serverId)?.[1];
+      const roleId = Object.entries(modRoleList).find(([key, val]) => key === serverId)?.[1];
 
 
-                if(!checkOwner){
-                        if(!(checkStaff.roles.cache.has(roleId))){
-                                await message.reply('you must be an owner/cowner of this channel to run this command')
-                                return;
+      if(!checkOwner){
+              if(!(checkStaff.roles.cache.has(roleId))){
+                      await message.reply('you must be an owner/cowner of this channel to run this command')
+                      return;
 
-                        }else if(checkStaff.roles.cache.has(roleId)){
-                                console.log("Ban Ran In: ", message.channel.id, "by", message.author.id)
-                        }
-                }
+              } else if(checkStaff.roles.cache.has(roleId)){
+                      console.log("Ban Ran In: ", message.channel.id, "by", message.author.id)
+              }
+      }
 
-	 	let messageContent = message.content.toString().toLowerCase();
-                let messageContentSplit = messageContent.split(" ");
-                let userName = message.mentions.users.first();
-                let id;
-                if(!userName){
-                        if(Number.isInteger(Number(messageContentSplit[0]))){
-                                id = messageContentSplit[0]
-                        }else{
-                                await message.reply("please specify a valid userid or username")
-                                return;
-                        }
-                }else if(userName) {
-                        id = message.mentions.users.first().id
-                }
+	  	let messageContent = message.content.toString().toLowerCase();
+      let messageContentSplit = messageContent.split(" ");
+      let userName = message.mentions.users.first();
+      let id;
+      if(!userName){
+              if(Number.isInteger(Number(messageContentSplit[0]))){
+                      id = messageContentSplit[0]
+              }else{
+                      await message.reply("please specify a valid userid or username")
+                      return;
+              }
+      }else if(userName) {
+              id = message.mentions.users.first().id
+      }
 
 		
 			//get database info on channel
 
-	    	const channelInfo = await getisland(message.channel.id);
-            	const user = message.author.id
+      const channelInfo = await getisland(message.channel.id);
+            const user = message.author.id
 
-	    	const checkbans = await bannedusers(message.channel.id);
-	    	let cleanid = await id.replace(/\D/g, '');
-	    	const memberTarget = await message.guild.members.cache.get(cleanid)
+      const checkbans = await bannedusers(message.channel.id);
+      let cleanid = await id.replace(/\D/g, '');
+      const memberTarget = await message.guild.members.cache.get(cleanid)
 
-	   	 	// safety check for staff and server level bots  
-	        const unbannableBots: { [key: string]: string } = {
-                        "801822272113082389": "1234731796944650340", // epic park
-                        "1113339391419625572":"1234731796944650340", // epic wonderland
-                        "839731097473908767": "1234731796944650340", //BLACKSTONE STAFF
-                    };
+      // safety check for staff and server level bots
+        const unbannableBots: { [key: string]: string } = {
+                      "801822272113082389": "1234731796944650340", // epic park
+                      "1113339391419625572":"1234731796944650340", // epic wonderland
+                      "839731097473908767": "1234731796944650340", //BLACKSTONE STAFF
+                  };
+      const botId = Object.entries(unbannableBots).find(([key, val]) => key === serverId)?.[1];
 
-                const botId = Object.entries(unbannableBots).find(([key, val]) => key === serverId)?.[1];
+      if (memberTarget.roles.cache.has(roleId)) {
+        await message.reply('Nice. You cant ban a staff member')
+          return;
+      }else if(cleanid == botId){
+      await message.reply('you can not ban server bots')
+      return;
+      }else if(memberTarget.roles.cache.has("1140520446241034290")){
+      await message.reply('you can not ban server bots')
+        return;
+          }
+      if(id  === message.author.id){
+                  await message.reply("Seriously? <a:ep_bozabonk:1164312811468496916>")
+      return;
+      }
+      // check if user is banned already
+      const isBanned = checkbans && checkbans.some((banned) => banned.user === cleanid)
 
-	    	if (memberTarget.roles.cache.has(roleId)) {
-			    await message.reply('Nice. You cant ban a staff member')
-		    		return;
-		    }else if(cleanid == botId){
-				await message.reply('you can not ban server bots')
-				return;
-	    	}else if(memberTarget.roles.cache.has("1140520446241034290")){   
-				await message.reply('you can not ban server bots') 
-					return;
-           	} 
-	    	if(id  === message.author.id){
-                	  await message.reply("Seriously? <a:ep_bozabonk:1164312811468496916>")
-		  	return;
-	    	}
-	   		// check if user is banned already 
-	    	const isBanned = checkbans && checkbans.some((banned) => banned.user === cleanid) 
-
-	    	if(isBanned) {
-			    await message.reply('user is already banned')
-		    	return;
-	    	} else {
-	    		await banuser(cleanid, message.channel.id)
-			await removeuser(cleanid, message.channel.id)
-	        	await message.channel.permissionOverwrites.edit(cleanid, {ViewChannel:false});
-	    	}
+      if(isBanned) {
+        await message.reply('user is already banned')
+        return;
+      } else {
+        await banuser(cleanid, message.channel.id)
+    await removeuser(cleanid, message.channel.id)
+          await message.channel.permissionOverwrites.edit(cleanid, {ViewChannel:false});
+      }
 			
-			//generate embed response
-	    	let banlist = " "
-	    	const allbans = await bannedusers(message.channel.id);
-	    
-	    	for (let i = 0;i < allbans.length; i++) {
-                        banlist = await banlist.concat(`\n> ${i+1}. <@!${allbans[i].user}>`)
-                }
-	    
-	    	let embed1 = new EmbedBuilder()
-	   		.setTitle("Channel Manager: Ban Channel User")
-			.setDescription(`__Current List of Banned Users__
-				${banlist}
-				*to unban a user, use command ep unban*`)
-			.setColor(`#097969`)
+    //generate embed response
+    let banlist = " "
+    const allbans = await bannedusers(message.channel.id);
+      for (let i = 0;i < allbans.length; i++) {
+                      banlist = await banlist.concat(`\n> ${i+1}. <@!${allbans[i].user}>`)
+              }
 
-	   		await message.reply({embeds:[embed1]})
+    let embed1 = new EmbedBuilder()
+      .setTitle("Channel Manager: Ban Channel User")
+      .setDescription(`__Current List of Banned Users__
+      ${banlist}
+      *to unban a user, use command ep unban*`)
+      .setColor(`#097969`)
+
+    await message.reply({embeds:[embed1]})
+
 	 }catch(err)
          {console.log(err)}
     },
--- src/commands/chat/staff_user_kick.ts
+++ src/commands/chat/staff_user_kick.ts
@@ -2,84 +2,104 @@ import { TextChannel, Message, ChannelType, EmbedBuilder } from "discord.js";
 import { PrefixCommand } from '../../handler';
 const { guildBan } = require('/home/ubuntu/ep_bot/extras/functions');
 
-
 export default new PrefixCommand({
-    name: "kickuser",
-    aliases: ["ku", "kick"],
-    // 1113339391419625572 - Epic Wonderland
-    // 1135995107842195550 - Epic Park
-    // 839731097473908767 - Blackstone
-    allowedGuilds: ['1135995107842195550','1113339391419625572', '839731097473908767','871269916085452870'],
-    allowedRoles:["1148992217202040942", // epic park staff
-                    "1113407924409221120", // epic staff
-                    '845499229429956628', // Blackstone Staff
-        '871393325389844521', // Luminescent Leiutenint
+  name: "kickuser",
+  aliases: ["ku", "kick"],
+  // 1113339391419625572 - Epic Wonderland
+  // 1135995107842195550 - Epic Park
+  // 839731097473908767 - Blackstone
+  allowedGuilds: ['1135995107842195550','1113339391419625572', '839731097473908767','871269916085452870'],
+  allowedRoles: [
+    "1148992217202040942", // epic park staff
+    "1113407924409221120", // epic staff
+    '845499229429956628', // Blackstone Staff
+    '871393325389844521', // Luminescent Lieutenant
+  ],
+  async execute(message: Message): Promise<void> {
+    try {
+      if (message.channel.type !== ChannelType.GuildText) return;
 
-    ],
-    async execute(message: Message): Promise<void> {
-        try{
-            let reason;
-            let messageContent = message.content.toString().toLowerCase()
-            let messageContentSplit = messageContent.split(" ");
-            let buildReason;
-            let serverId = message.guild.id
-            if(message.channel.type !== ChannelType.GuildText) return;
+      const content = message.content.trim();
+      // Split content into prefix, command, target, and rest
+      const parts = content.split(/\s+/);
+      const commandName = parts[1]?.toLowerCase();
+      const target = parts[2];
+      const rest = parts.slice(3);
 
-             const mentionedUser = message.mentions.users.first();
+      console.log(content);
+      console.log(commandName);
+      console.log(target);
 
-            if (!mentionedUser) {
-                await message.channel.send('Please mention a user to kick.');
-                return;
-            }
+      if (!target) {
+        await message.channel.send('Please specify a user to kick (mention or ID).');
+        return;
+      }
 
-            const member = message.guild?.members.cache.get(mentionedUser.id);
+      // Determine target user ID
+      const mentionMatch = target.match(/^<@!?(\d+)>$/);
+      let userId: string | null = null;
 
-            buildReason = messageContentSplit.slice(1)
-            reason = String(buildReason).replaceAll(",", " ")
+      if (mentionMatch) {
+        userId = mentionMatch[1];
+      } else if (/^\d{17,19}$/.test(target)) {
+        userId = target;
+      } else {
+        await message.channel.send('Please provide a valid user mention or user ID.');
+        return;
+      }
 
-            if (!member) {
-                await message.channel.send('User not found in this server.');
-                return;
-            }
+      // Fetch member from guild
+      const member = await message.guild?.members.fetch(userId).catch(() => null);
+      if (!member) {
+        await message.channel.send('User not found in this server.');
+        return;
+      }
 
-            if (reason.length === 0) {
-                reason = "no reason supplied"
-            }
+      // Build reason from rest of message
+      const reason = rest.join(' ').trim() || 'no reason supplied';
 
-            let date = new Date().toLocaleString()
-            date = String(date.replace(',', ""))
+      // Kick the member
+      await member.kick(reason);
+      await message.channel.send(`Kicked ${member.user.tag} successfully.`);
 
-            await member.kick();
-            await message.channel.send(`Kicked ${mentionedUser.tag} successfully.`);
+      // Log the action in database or audit
+      const date = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+      await guildBan(member, 'kick', reason, message.author.id, date);
 
-            await guildBan(member, 'kick', reason, message.author.id, String(date))
+      // Prepare embed for logs
+      const embed = new EmbedBuilder()
+        .setTitle('Staff Manager: Kick User')
+        .setDescription(
+          `**Action**: Kicked User\n` +
+          `**User**: ${member.user.tag} (${member.id})\n` +
+          `**Reason**: ${reason}\n` +
+          `**Date**: ${date}\n` +
+          `**Set By**: ${message.author.tag}`
+        )
+        .setColor('DarkRed');
 
-            let embed = new EmbedBuilder()
-                .setTitle("Staff Manager: Kick User")
-                .setDescription(`**Action**: Kicked User
-                                    **User**: ${member}
-                                    **Reason**: ${reason}
-                                    **Date:** ${String(date)}
-                                    **Set By:** ${message.author.username}`)
+      // Map of guild IDs to log channel IDs
+      const banChannelMap: Record<string, string> = {
+        '1135995107842195550': '1160751610771820554', // epic park
+        '1113339391419625572': '1115941478007582740', // epic wonderland staff
+        '839731097473908767': '839731097754533897', // blackstone warn logs
+        '871269916085452870': '997111935952748575', // Luminescent Admin Logs
+      };
 
-            const banChannel: { [key: string]: string } = {
-                "1135995107842195550": "1160751610771820554", // epic park
-                '1113339391419625572':'1115941478007582740', // epic wonderland staff
-                "839731097473908767": "839731097754533897", // blackstone warn logs
-              "871269916085452870": "997111935952748575" //Luminescent Admin Logs
-            };
+      const logChannelId = banChannelMap[message.guild.id];
+      if (logChannelId) {
+        const logChannel = message.guild.channels.cache.get(logChannelId) as TextChannel;
+        if (logChannel) await logChannel.send({ embeds: [embed] });
+      }
 
-            const getBanChannel = Object.entries(banChannel).find(([key, val]) => key === serverId)?.[1];
+      // Reply with embed
+      await message.reply({ embeds: [embed] });
 
-            const banlog = await message.guild.channels.cache.find(channel => channel.id === getBanChannel) as TextChannel;
-                await banlog.send({embeds: [embed]})
-                await message.reply({embeds: [embed]})
-
-
-        } catch (error) {
-            console.error(error);
-	    if(message.channel.type !== ChannelType.GuildText) return;
-            await message.channel.send('Failed to kick the user. Ensure the bot has the correct permissions.');
-        }
+    } catch (error) {
+      console.error('Kick command error:', error);
+      if (message.channel.type === ChannelType.GuildText) {
+        await message.channel.send('Failed to kick the user. Ensure the bot has the correct permissions and role hierarchy.');
+      }
     }
+  }
 });
